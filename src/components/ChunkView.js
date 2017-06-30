95719731b5b0c258e6c49bfb4d9ec59a6b6d0689
Lauri Keisanen <lauri.keisanen@aalto.fi>
2017-06-30 11:59:25 +0300

    Add option to skip forward before time is out

--- src/components/ChunkView.js
+++ src/components/ChunkView.js
@@ -15,17 +15,29 @@ export default class ChunkView extends React.Component {
 
     componentWillMount() {
         this.remainingTimer = setInterval(this.tick, this.tickrate);
+        document.addEventListener("keydown", this.handleKeyDown)
     }
 
     tick = () => {
         const remainingTime = this.state.remainingTime - this.tickrate;
         this.setState({remainingTime: remainingTime});
         if (remainingTime <= 0) {
-            clearInterval(this.remainingTimer);
-            // We don't want to immediately jump to inputting the word; only after a little delay
-            this.setState({showText: false});
-            setTimeout(this.props.onComplete, 2000)
+            this.complete()
         }
+    };
+
+    handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            this.complete()
+        }
+    };
+
+    complete = () => {
+        document.removeEventListener("keydown", this.handleKeyDown);
+        clearInterval(this.remainingTimer);
+        // We don't want to immediately jump to inputting the word; only after a little delay
+        this.setState({showText: false});
+        setTimeout(this.props.onComplete, 2000)
     };
 
     render() {
@@ -45,6 +57,7 @@ export default class ChunkView extends React.Component {
                 <div className="progressbar">
                     <div className="progress" style={style}></div>
                 </div>
+                <div className="helpText">press enter to skip forward</div>
             </div>;
 
         return this.state.showText ? layout : <div>. . .</div>
