408a6d76b029f364ccba740bbb0e29ca2a48b80c
Lauri Keisanen <lauri.keisanen@aalto.fi>
2017-07-31 15:29:11 +0300

    Add logging module
    Main purpose is to send data to a server for analysing.

---
+++ src/modules/Logging.js
@@ -0,0 +1,44 @@
+import 'whatwg-fetch'
+import { randomWord } from './ChunkModule'
+
+const api = "http://localhost:5000/api";
+
+function getUserID() {
+    if (localStorage.getItem("userID")) {
+        return localStorage.getItem("userID");
+    }
+    const userID = randomWord(10);
+    localStorage.setItem("userID", userID);
+    return userID
+}
+
+function getSessionID() {
+    if (sessionStorage.getItem("sessionID")) {
+        return sessionStorage.getItem("sessionID");
+    }
+    const sessionID = randomWord(10);
+    sessionStorage.setItem("sessionID", sessionID);
+    return sessionID
+}
+
+// Maybe we should retry / exponential backoff on error?
+export function send(eventType, data={}) {
+    fetch(api + "/event", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            userID: getUserID(),
+            sessionID: getSessionID(),
+            eventType,
+            data
+        })
+    }).then((response) => {
+        if (!response.ok) {
+            console.error(`Server response was not ok: ${response.status}, ${response.statusText}`);
+        }
+    }).catch((error) => {
+        console.error(`Network error: ${error.message}`);
+    })
+}
